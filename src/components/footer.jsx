755421d4545533bc73403eb8e4b9116b1da84076
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-06-29 18:57:17 +0530

    add footer.jsx

---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+
+    return (
+        <footer class="footer mt-5 py-3 bg-dark">
+            <div class="container">
+                <span class="text-light"><center>Fidenz Academy - 2023</center></span>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
