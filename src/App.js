43ecf2bd458feddb110b64e82edd2fe9f3f6b10c
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-06-30 00:59:04 +0530

    add all components together

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import Header from './components/header';
+
+import Footer from './components/footer';
+
+import Weather from './components/weather';
+
+import Finder from './components/finder';
+
 import './App.css';
 
-function App() {
+
+function App(){
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+
+      <Finder />
+
+      <Weather />
+      
+      <Footer />
     </div>
-  );
+  )
 }
 
 export default App;
