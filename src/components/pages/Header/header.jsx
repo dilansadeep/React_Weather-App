4cabed9223a5067ae0fa5c88d79f9bff014a19a9
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-07-02 10:30:03 +0530

    move that components into separate folder

---
+++ src/components/pages/Header/header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header() {
+
+    return (
+        <div class="container">
+          
+          <header class="text-center p-4 p-lg-5">
+            
+            <img src={`https://i.ibb.co/svfwpw9/3.png`} alt="weather-icon" />
+            
+          </header>
+      
+      </div>
+    );
+}
+
+export default Header;
+
